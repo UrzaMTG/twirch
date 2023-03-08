66fbfdbac00332d652e09ba1e2ad97d04f52aed2
UrzaMTG <urzamtg@gmail.com>
2023-03-07 18:34:49 -0600

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -10,20 +10,30 @@ const io = new Server(server);
 const tmi = require('tmi.js');
 
 const client = new tmi.Client({
-	options: { skipMembership: true }
+  options: {
+    skipMembership: true,
+    debug: true,
+  },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
 });
 
-client.connect();
+client.connect().catch((error) => {
+  console.error(error);
+});
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 client.on('message', (channel, tags, message, self) => {
-	// Ignore echoed messages.
-	if(self) return;
+  // Ignore echoed messages.
+  if (self) return;
 
-	// Echo message to clients that care about this channel
+  // Echo message to clients that care about this channel
 });
 
 /*io.on('connection', (socket) => {
