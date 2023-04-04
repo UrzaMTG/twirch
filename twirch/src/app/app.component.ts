3e4256801ed401f666c7bc2a45e4ba91048cffdd
Michael McBride <urzamtg@gmail.com>
2023-04-03 21:09:00 -0500

    Timestamp support

--- twirch/src/app/app.component.ts
+++ twirch/src/app/app.component.ts
@@ -21,11 +21,26 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this._msgSub = this.channelService.chatMessage.subscribe(msg => { this.messages.push(msg); })
+    this._msgSub = this.channelService.chatMessage.subscribe((msg) => this.processMessage(msg));
   }
   
   ngOnDestroy(): void {
     this._msgSub?.unsubscribe();
+  }
+
+  private processMessage(msg: Message): void {
+    // Add timestamp
+    msg.timestamp = new Date(Date.now());
+
+    // Replace emotes
+    // https://static-cdn.jtvnw.net/emoticons/v1/[emote_id]/1.0
+
+
+    // Strip/escape HTML
+
+
+    // Push to bound array
+    this.messages.push(msg);
   }
     
 }
