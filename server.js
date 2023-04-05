af4a1d9ef11c48b2fd9cb88f36d944bc6cfed369
Michael McBride <urzamtg@gmail.com>
2023-04-04 21:58:08 -0500

    Remove extra variable

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express(),
-      port = process.env.PORT || 3080,
-      clientPort = 4200;
+      port = process.env.PORT || 3080
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
