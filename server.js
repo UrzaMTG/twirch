2218febc1f3b780521b12abb4c4c1d02711f71dc
Michael McBride <urzamtg@gmail.com>
2023-04-04 22:59:34 -0500

    I missed a semicolon, the horror!

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express(),
-      port = process.env.PORT || 3080
+      port = process.env.PORT || 3080;
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
