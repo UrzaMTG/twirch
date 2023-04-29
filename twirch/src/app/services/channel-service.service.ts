c2ac5796579d0b67328c781d083df159ba264567
Michael McBride <urzamtg@gmail.com>
2023-04-29 01:25:23 -0500

    Temporary debugging code

--- twirch/src/app/services/channel-service.service.ts
+++ twirch/src/app/services/channel-service.service.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { Message } from '../models/message';
 
+enum SocketIoManagerEvents {
+  Error = 'error',
+  Ping = 'ping',
+  Reconnect = 'reconnect',
+  ReconnectAttempt = 'reconnect_attempt',
+  ReconnectError = 'reconnect_error',
+  ReconnectFailed = 'reconnect_failed'
+}
+
 const _joinChannels: string = 'join channels';
 const _chatMessage: string = 'chat message';
 const _keepAlive: string = 'keepAlive';
@@ -16,6 +25,12 @@ export class ChannelService {
 
   constructor(private socket: Socket) {
     this.keepAliveTimer = setInterval(() => { this.keepAlive }, 60000);
+    this.socket.on(SocketIoManagerEvents.Error, () => {console.log("Socket error")});
+    this.socket.on(SocketIoManagerEvents.Ping, () => {console.log("Ping received")});
+    this.socket.on(SocketIoManagerEvents.Reconnect, () => {console.log("Socket reconnecting")});
+    this.socket.on(SocketIoManagerEvents.ReconnectAttempt, () => {console.log("Socket reconnect attempt")});
+    this.socket.on(SocketIoManagerEvents.ReconnectError, () => {console.log("Socket reconnect error")});
+    this.socket.on(SocketIoManagerEvents.ReconnectFailed, () => {console.log("Socket reconnect failure")});
   }
 
   selectChannels(channels: string[]): void {
