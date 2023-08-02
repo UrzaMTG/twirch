29cb9635669ff474a4a3e328cb0bfd490fd7e73d
Michael McBride <urzamtg@gmail.com>
2023-08-01 20:12:11 -0500

    Switch to new badge API Fixes #42

--- src/app/models/BadgeData.ts
+++ src/app/models/BadgeData.ts
@@ -1,12 +1,27 @@
 export interface BadgeData {
-  badge_sets: {[id: string]: FullBadge};
+  data: FullBadge[];
 }
 
 export interface FullBadge {
+  set_id: string;
+  versions: BadgeVersion[];
+}
+
+export interface BadgeDict {
+  [id: string]: ProperBadge;
+}
+
+export interface ProperBadge {
   versions: {[version: string]: BadgeVersion};
 }
 
 export interface BadgeVersion {
+  id: string;
   image_url_1x: string;
+  image_url_2x: string;
+  image_url_4x: string;
   title: string;
+  description: string;
+  click_action: string | null;
+  click_url: string | null;
 }
--- src/app/services/Badges.service.ts
+++ src/app/services/Badges.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Badges } from 'tmi.js';
-import { BadgeData } from '../models/BadgeData';
+import { BadgeData, BadgeDict } from '../models/BadgeData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BadgeService {
-  private data?: BadgeData;
+  private badgeData: BadgeDict = {};
 
   constructor(private http: HttpClient) {
-    const url = 'https://badges.twitch.tv/v1/badges/global/display';
-    this.http.get<BadgeData>(url).subscribe( (res) => {
-      this.data = res;
+    const url = 'https://api.twitch.tv/helix/chat/badges/global';
+    const headers = new HttpHeaders()
+      .set('Authorization', 'Bearer cr2x9xzuef13wlwrs29tm2wq71lsuj')
+      .set('Client-Id', '7p63vvqitomsz4k6pig8gi1ym2wv6s');
+    this.http.get<BadgeData>(url, {headers: headers}).subscribe( (res) => {
+      res.data.map(badge => {
+        const properBadge = {
+          versions: Object.fromEntries(badge.versions.map(version => [version.id, version]))
+        };
+        this.badgeData[badge.set_id] = properBadge
+      });
     })
   }
 
@@ -25,27 +33,27 @@ export class BadgeService {
   parseBadges(badges: Badges): string {
     let retVal = "";
 
-    if (this.data) {
+    if (this.badgeData) {
       for (const [k, v] of Object.entries(badges)) {
         if (v) {
           if (k === 'subscriber') {
             let maxMonths: number = 0;
             let msgMonths = Number.parseInt(v);
-            for (const [months, v2] of Object.entries(this.data.badge_sets[k].versions)) {
+            for (const [months, v2] of Object.entries(this.badgeData[k].versions)) {
               let monthNum: number = Number.parseInt(months);
               maxMonths = Math.max(maxMonths, monthNum);
               if (monthNum === msgMonths) {
-                retVal += `<img src="${this.data.badge_sets[k].versions[months].image_url_1x}" class="twitch-badge" title="${this.data.badge_sets[k].versions[months].title}" />`;
+                retVal += `<img src="${this.badgeData[k].versions[months].image_url_1x}" class="twitch-badge" title="${this.badgeData[k].versions[months].title}" />`;
                 break;
               }
             }
 
             if (msgMonths > maxMonths) {
-              retVal += `<img src="${this.data.badge_sets[k].versions[maxMonths].image_url_1x}" class="twitch-badge" title="${this.data.badge_sets[k].versions[maxMonths].title}" />`;
+              retVal += `<img src="${this.badgeData[k].versions[maxMonths].image_url_1x}" class="twitch-badge" title="${this.badgeData[k].versions[maxMonths].title}" />`;
             }
           }
           else {
-            retVal += `<img src="${this.data.badge_sets[k].versions[v].image_url_1x}" class="twitch-badge" title="${this.data.badge_sets[k].versions[v].title}" />`;
+            retVal += `<img src="${this.badgeData[k].versions[v].image_url_1x}" class="twitch-badge" title="${this.badgeData[k].versions[v].title}" />`;
           }
         }
       }
